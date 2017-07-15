611aa8c826172e08088fcb1725713f68c3ad42eb
Adel Acebal <adelacebal@yahoo.com>
2017-07-15 15:42:31 -0400

    more angular work

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -7,6 +9,10 @@ import { HomePageComponent } from './home-page/home-page.component';
 import { ListsPageComponent } from './lists-page/lists-page.component';
 import { LogInComponent } from './log-in/log-in.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
+
+import { SessionService } from './services/session.service';
+import { ListService } from './services/list.service';
+import { CardService } from './services/card.service';
 
 @NgModule({
   declarations: [
@@ -20,7 +26,11 @@ import { SignUpComponent } from './sign-up/sign-up.component';
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    SessionService,
+    ListService,
+    CardService
+],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/services/session.service.ts
+++ src/app/services/session.service.ts
@@ -1,8 +1,20 @@
 import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/toPromise';
 
 @Injectable()
 export class SessionService {
 
-  constructor() { }
+  baseUrl: string = 'http://localhost:3000';
+
+  constructor(
+    private http: Http
+  ) { }
+
+  checkLogin() {
+    return this.http.get('baseURL/checklogin')
+      .toPromise()
+      .then(res => res.json());
+  }
 
 }
