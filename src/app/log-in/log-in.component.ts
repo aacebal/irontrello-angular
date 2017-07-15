e5514586bff598110a48bc3426c93a60ed656746
Adel Acebal <adelacebal@yahoo.com>
2017-07-15 16:27:24 -0400

    login continued

--- src/app/log-in/log-in.component.ts
+++ src/app/log-in/log-in.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 
+import { SessionService } from '../services/session.service';
+import { Router } from '@angular/router';
+
 @Component({
   selector: 'app-log-in',
   templateUrl: './log-in.component.html',
   styleUrls: ['./log-in.component.css']
 })
 export class LogInComponent implements OnInit {
+  formEmail: string;
+  formPassword: string;
 
-  constructor() { }
+  errorMessage: string;
+
+  constructor( private session: SessionService, private router: Router ) { }
 
   ngOnInit() {
+  }
+
+  submitLogin() {
+    this.session.login(this.formEmail, this.formPassword)
+      .then(() => {
+        this.router.navigate(['/lists']);
+      })
+      .catch((errResponse) => {
+        const apiInfo = errResponse.json();
+        this.errorMessage = apiInfo.message;
+      })
   }
 
 }
